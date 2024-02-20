7ac9f07157a7305cf9cc68a66f43627e415836bc
navaghandabhi <navghandabhi@megamindsunited.com>
2024-02-20 17:07:42 +0530

    add fonts to i card

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='name'>Navaghan</h1>
+        <h1 className='name'>Navaghan Dabhi</h1>
         <h4 className='position'>flutter Developer</h4>
         <p className='email'>dabhinavaghan6@gmail.com</p>
       </header>
