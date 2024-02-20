dd56832270ff347f308c25916001d3c6c4f31ab6
navaghandabhi <navghandabhi@megamindsunited.com>
2024-02-20 16:12:06 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='name'>Navaghan</h1>
+        <h4 className='position'>flutter Developer</h4>
+        <p className='email'>dabhinavaghan6@gmail.com</p>
       </header>
     </div>
   );
